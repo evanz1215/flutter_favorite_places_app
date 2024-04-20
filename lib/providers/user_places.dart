9dee5407d2eddcf49a2606a863e8e9da8b5b1648
Evan <evanz1215@gmail.com>
2024-04-21 02:17:21 +0800

    Update imports and refactor AddPlaceScreen to use Flutter Riverpod for state management

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_favorite_places_app/models/place.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
